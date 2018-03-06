44c213bcbc5ea673ca121b6a1187e5a162828cd4
mpelletiervaillant <melany.pelletier@gmail.com>
2018-03-06 09:01:02 -0500

    Fix indentation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-$('.info-panel-btn').on('click', function() {
-var id = $(this).attr('href');
+$('.info-panel-btn').on('click', function () {
+  var id = $(this).attr('href');
   var isOpen = $(this).hasClass('info-panel-btn-active');
-$('.info-panel-btn').removeClass('info-panel-btn-active');
-$('.info-panel').attr('hidden', true);
+
+  $('.info-panel-btn').removeClass('info-panel-btn-active');
+  $('.info-panel').attr('hidden', true);
   if (!isOpen) {
     $(id).removeAttr('hidden');
     $(this).addClass('info-panel-btn-active');
