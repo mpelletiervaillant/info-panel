7ff890105341ca6d58db15247b36e78fff8f168f
mpelletiervaillant <melany.pelletier@gmail.com>
2018-03-06 08:59:36 -0500

    Wrote all the code

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.info-panel-btn').on('click', function() {
+var id = $(this).attr('href');
+  var isOpen = $(this).hasClass('info-panel-btn-active');
+$('.info-panel-btn').removeClass('info-panel-btn-active');
+$('.info-panel').attr('hidden', true);
+  if (!isOpen) {
+    $(id).removeAttr('hidden');
+    $(this).addClass('info-panel-btn-active');
+  }
+});
